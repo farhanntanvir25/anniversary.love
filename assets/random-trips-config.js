d4e2286742bf3298f0c8520ce1a45e4d3be57232
farhanntanvir25 <farhanntanvir@gmail.com>
2026-09-17 23:29:16 +0600

    Add files via upload

---
+++ assets/random-trips-config.js
@@ -0,0 +1,3 @@
+// Random Trips Google Drive configuration
+// Drive folder is public/viewable; this key is restricted in Google Cloud Console.
+window.RANDOM_TRIPS_API_KEY = 'AIzaSyD3XD0b7gezc3yfN8vvQtsmlPwX_F1nFQE';
